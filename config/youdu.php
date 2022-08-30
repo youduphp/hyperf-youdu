71fa4db923fef9b0baa54834756c5c775c1a3909
huangdijia <huangdijia@gmail.com>
2022-08-30 20:54:00 +0800

    Fix test

--- config/youdu.php
+++ config/youdu.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 /**
- * This file is part of laravel-youdu.
+ * This file is part of hyperf-youdu.
  *
- * @link     https://github.com/youduphp/laravel-youdu
- * @document https://github.com/youduphp/laravel-youdu/blob/main/README.md
+ * @link     https://github.com/youduphp/hyperf-youdu
+ * @document https://github.com/youduphp/hyperf-youdu/blob/main/README.md
  * @contact  huangdijia@gmail.com
  */
 return [
---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of hyperf-youdu.
+ *
+ * @link     https://github.com/youduphp/hyperf-youdu
+ * @document https://github.com/youduphp/hyperf-youdu/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+test('example', function () {
+    expect(true)->toBeTrue();
+});
