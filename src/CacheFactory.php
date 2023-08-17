99fade4b01ac6b4ba8586e740280b5df72b957f2
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-08-17 10:35:37 +0800

    Cs Fix

--- src/CacheFactory.php
+++ src/CacheFactory.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
  * @document https://github.com/youduphp/hyperf-youdu/blob/main/README.md
  * @contact  huangdijia@gmail.com
  */
+
 namespace YouduPhp\HyperfYoudu;
 
 use Psr\Container\ContainerInterface;
